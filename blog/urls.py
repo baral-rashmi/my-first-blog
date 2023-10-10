28ee267cbd6ece2f6990eb7277e199bae0cfb974
Rashmi Baral <rbaral@ivytech.edu>
2023-10-10 12:17:00 -0400

    Added a page for unpublished post,authorization for  only logged in users.

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from blog import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,16 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog import views
+from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('drafts/', views.post_draft_list, name='post_draft_list'),
-    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
-    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('', include('blog.urls'))
 ]
